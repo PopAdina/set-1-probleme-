820603bc605cef0e790c389a0032fad551ae7762
Adina Pop <147972913+PopAdina@users.noreply.github.com>
2024-01-18 23:11:12 +0200

    Add files via upload

---
+++ pb18/pb18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un număr: ");
+            int n = int.Parse(Console.ReadLine());
+            int factor = 2;
+            Console.Write($"Descompunerea în factori primi ai lui {n} este: ");
+            while (n > 1)
+            {
+                if (n % factor == 0)
+                {
+                    int count = 0;
+                    while (n % factor == 0)
+                    {
+                        n /= factor;
+                        count++;
+                    }
+                    Console.Write($"{factor}^{count}");
+                    if (n > 1)
+                    {
+                        Console.Write(" x ");
+                    }
+                }
+                factor++;
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pb19/pb19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar: ");
+            int num = int.Parse(Console.ReadLine());
+            bool hasOnlyTwoRepeatingDigits = CheckForTwoRepeatingDigits(num);
+            if (hasOnlyTwoRepeatingDigits)
+            {
+                Console.WriteLine($"{num} este format doar din doua cifre care se pot repeta.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"{num} nu este format doar din doua cifre care se pot repeta.");
+                Console.ReadLine();
+            }
+        }
+        static bool CheckForTwoRepeatingDigits(int num)
+        {
+            string numStr = num.ToString();
+            int distinctDigitCount = numStr.Distinct().Count();
+            return distinctDigitCount == 2;
+        }
+    }
+}
