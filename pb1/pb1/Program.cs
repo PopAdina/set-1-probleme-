256e4b08d45ed0da45c35e7b21a375e0ffb97db0
Adina Pop <147972913+PopAdina@users.noreply.github.com>
2024-01-18 23:05:29 +0200

    Add files via upload

---
+++ pb1/pb1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float  a, b, x;
+            Console.WriteLine("a=");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            b = float.Parse(Console.ReadLine());
+            if (a != 0)
+            {
+                x = -b / a;
+                Console.WriteLine(" x= " + x);
+            }
+            else { if ( b == 0)
+                { Console.WriteLine(" ecuatia are o infinitate de solutii");}
+                   else { Console.WriteLine(" ecuatia nu are solutii reale"); }
+            }
+             Console.ReadLine();
+        }
+    }
+}
